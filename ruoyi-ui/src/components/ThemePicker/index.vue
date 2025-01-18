b09d56d76c34ca6646a13429dc5c79fc45d44fe5
chip-cmyk <290806494@qq.com>
2025-01-18 18:39:18 +0800

    改默认样式，侧栏hover颜色跟随主题色，navbar，tagsview，替换图片图标

--- ruoyi-ui/src/components/ThemePicker/index.vue
+++ ruoyi-ui/src/components/ThemePicker/index.vue
@@ -1,155 +1,221 @@
 <template>
   <el-color-picker
     v-model="theme"
-    :predefine="['#409EFF', '#1890ff', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
+    :predefine="[
+      '#409EFF',
+      '#1890ff',
+      '#304156',
+      '#212121',
+      '#11a983',
+      '#13c2c2',
+      '#6959CD',
+      '#f5222d',
+    ]"
     class="theme-picker"
     popper-class="theme-picker-dropdown"
   />
 </template>
 
 <script>
-const ORIGINAL_THEME = '#409EFF' // default color
+const ORIGINAL_THEME = "#409EFF"; // default color
 
 export default {
   data() {
     return {
-      chalk: '', // content of theme-chalk css
-      theme: ''
-    }
+      chalk: "", // content of theme-chalk css
+      theme: "",
+    };
   },
   computed: {
     defaultTheme() {
-      return this.$store.state.settings.theme
-    }
+      return this.$store.state.settings.theme;
+    },
+    defaultSideTheme() {
+      return this.$store.state.settings.sideTheme;
+    },
   },
   watch: {
     defaultTheme: {
-      handler: function(val, oldVal) {
-        this.theme = val
+      handler: function (val, oldVal) {
+        this.theme = val;
       },
-      immediate: true
+      immediate: true,
     },
     async theme(val) {
-      await this.setTheme(val)
-    }
+      await this.setTheme(val);
+    },
+    async defaultSideTheme() {
+      await this.updateSideHoverStyle(this.defaultTheme);
+    },
   },
   created() {
-    if(this.defaultTheme !== ORIGINAL_THEME) {
-      this.setTheme(this.defaultTheme)
+    if (this.defaultTheme !== ORIGINAL_THEME) {
+      this.setTheme(this.defaultTheme);
     }
   },
   methods: {
     async setTheme(val) {
-      const oldVal = this.chalk ? this.theme : ORIGINAL_THEME
-      if (typeof val !== 'string') return
-      const themeCluster = this.getThemeCluster(val.replace('#', ''))
-      const originalCluster = this.getThemeCluster(oldVal.replace('#', ''))
+      const oldVal = this.chalk ? this.theme : ORIGINAL_THEME;
+      if (typeof val !== "string") return;
+      const themeCluster = this.getThemeCluster(val.replace("#", ""));
+      const originalCluster = this.getThemeCluster(oldVal.replace("#", ""));
 
       const getHandler = (variable, id) => {
         return () => {
-          const originalCluster = this.getThemeCluster(ORIGINAL_THEME.replace('#', ''))
-          const newStyle = this.updateStyle(this[variable], originalCluster, themeCluster)
+          const originalCluster = this.getThemeCluster(
+            ORIGINAL_THEME.replace("#", "")
+          );
+          const newStyle = this.updateStyle(
+            this[variable],
+            originalCluster,
+            themeCluster
+          );
 
-          let styleTag = document.getElementById(id)
+          let styleTag = document.getElementById(id);
           if (!styleTag) {
-            styleTag = document.createElement('style')
-            styleTag.setAttribute('id', id)
-            document.head.appendChild(styleTag)
+            styleTag = document.createElement("style");
+            styleTag.setAttribute("id", id);
+            document.head.appendChild(styleTag);
           }
-          styleTag.innerText = newStyle
-        }
-      }
+          styleTag.innerText = newStyle;
+        };
+      };
 
       if (!this.chalk) {
-        const url = `/styles/theme-chalk/index.css`
-        await this.getCSSString(url, 'chalk')
+        const url = `/styles/theme-chalk/index.css`;
+        await this.getCSSString(url, "chalk");
       }
 
-      const chalkHandler = getHandler('chalk', 'chalk-style')
-      chalkHandler()
+      const chalkHandler = getHandler("chalk", "chalk-style");
+      chalkHandler();
 
-      const styles = [].slice.call(document.querySelectorAll('style'))
-        .filter(style => {
-          const text = style.innerText
-          return new RegExp(oldVal, 'i').test(text) && !/Chalk Variables/.test(text)
-        })
-      styles.forEach(style => {
-        const { innerText } = style
-        if (typeof innerText !== 'string') return
-        style.innerText = this.updateStyle(innerText, originalCluster, themeCluster)
-      })
+      const styles = [].slice
+        .call(document.querySelectorAll("style"))
+        .filter((style) => {
+          const text = style.innerText;
+          return (
+            new RegExp(oldVal, "i").test(text) && !/Chalk Variables/.test(text)
+          );
+        });
+      styles.forEach((style) => {
+        const { innerText } = style;
+        if (typeof innerText !== "string") return;
+        style.innerText = this.updateStyle(
+          innerText,
+          originalCluster,
+          themeCluster
+        );
+      });
 
-      this.$emit('change', val)
+      // 处理具有 theme-dependent 类名的元素
+      // const themeDependentElements = document.querySelectorAll(
+      //   ".el-submenu__title:hover,.el-submenu.el-menu-item:hover"
+      // );
+      // themeDependentElements.forEach((element) => {
+      //   element.style.backgroundColor = val + "1A"; // 10% 透明度，假设主题色为十六进制
+      //   element.style.backgroundColor = val + "1A!important"; // 添加!important 提高优先级
+      // });
+
+      // const hoverStyle = document.createElement("style");
+      // hoverStyle.innerHTML = `.el-menu-item:hover,.el-submenu__title:hover,.el-submenu.el-menu-item:hover { background-color: ${
+      //   val + "1A"
+      // }!important; }`;
+      // document.head.appendChild(hoverStyle);
+
+      this.updateSideHoverStyle(val);
+
+      this.$emit("change", val);
     },
 
     updateStyle(style, oldCluster, newCluster) {
-      let newStyle = style
+      let newStyle = style;
       oldCluster.forEach((color, index) => {
-        newStyle = newStyle.replace(new RegExp(color, 'ig'), newCluster[index])
-      })
-      return newStyle
+        newStyle = newStyle.replace(new RegExp(color, "ig"), newCluster[index]);
+      });
+      return newStyle;
     },
 
     getCSSString(url, variable) {
-      return new Promise(resolve => {
-        const xhr = new XMLHttpRequest()
+      return new Promise((resolve) => {
+        const xhr = new XMLHttpRequest();
         xhr.onreadystatechange = () => {
           if (xhr.readyState === 4 && xhr.status === 200) {
-            this[variable] = xhr.responseText.replace(/@font-face{[^}]+}/, '')
-            resolve()
+            this[variable] = xhr.responseText.replace(/@font-face{[^}]+}/, "");
+            resolve();
           }
-        }
-        xhr.open('GET', url)
-        xhr.send()
-      })
+        };
+        xhr.open("GET", url);
+        xhr.send();
+      });
     },
 
     getThemeCluster(theme) {
       const tintColor = (color, tint) => {
-        let red = parseInt(color.slice(0, 2), 16)
-        let green = parseInt(color.slice(2, 4), 16)
-        let blue = parseInt(color.slice(4, 6), 16)
+        let red = parseInt(color.slice(0, 2), 16);
+        let green = parseInt(color.slice(2, 4), 16);
+        let blue = parseInt(color.slice(4, 6), 16);
 
-        if (tint === 0) { // when primary color is in its rgb space
-          return [red, green, blue].join(',')
+        if (tint === 0) {
+          // when primary color is in its rgb space
+          return [red, green, blue].join(",");
         } else {
-          red += Math.round(tint * (255 - red))
-          green += Math.round(tint * (255 - green))
-          blue += Math.round(tint * (255 - blue))
+          red += Math.round(tint * (255 - red));
+          green += Math.round(tint * (255 - green));
+          blue += Math.round(tint * (255 - blue));
 
-          red = red.toString(16)
-          green = green.toString(16)
-          blue = blue.toString(16)
+          red = red.toString(16);
+          green = green.toString(16);
+          blue = blue.toString(16);
 
-          return `#${red}${green}${blue}`
+          return `#${red}${green}${blue}`;
         }
-      }
+      };
 
       const shadeColor = (color, shade) => {
-        let red = parseInt(color.slice(0, 2), 16)
-        let green = parseInt(color.slice(2, 4), 16)
-        let blue = parseInt(color.slice(4, 6), 16)
+        let red = parseInt(color.slice(0, 2), 16);
+        let green = parseInt(color.slice(2, 4), 16);
+        let blue = parseInt(color.slice(4, 6), 16);
 
-        red = Math.round((1 - shade) * red)
-        green = Math.round((1 - shade) * green)
-        blue = Math.round((1 - shade) * blue)
+        red = Math.round((1 - shade) * red);
+        green = Math.round((1 - shade) * green);
+        blue = Math.round((1 - shade) * blue);
 
-        red = red.toString(16)
-        green = green.toString(16)
-        blue = blue.toString(16)
+        red = red.toString(16);
+        green = green.toString(16);
+        blue = blue.toString(16);
 
-        return `#${red}${green}${blue}`
-      }
+        return `#${red}${green}${blue}`;
+      };
 
-      const clusters = [theme]
+      const clusters = [theme];
       for (let i = 0; i <= 9; i++) {
-        clusters.push(tintColor(theme, Number((i / 10).toFixed(2))))
+        clusters.push(tintColor(theme, Number((i / 10).toFixed(2))));
       }
-      clusters.push(shadeColor(theme, 0.1))
-      return clusters
-    }
-  }
-}
+      clusters.push(shadeColor(theme, 0.1));
+      return clusters;
+    },
+
+    updateSideHoverStyle(val) {
+      // 根据 sideTheme 判断是否为亮色主题
+      if (this.$store.state.settings.sideTheme === "theme-light") {
+        let hoverStyle = document.getElementById("hover-style");
+        if (!hoverStyle) {
+          hoverStyle = document.createElement("style");
+          hoverStyle.setAttribute("id", "hover-style");
+          document.head.appendChild(hoverStyle);
+        }
+        hoverStyle.innerHTML = `.el-menu-item:hover,.el-submenu__title:hover,.el-submenu.el-menu-item:hover { background-color: ${
+          val + "1A"
+        }!important; }`;
+      } else {
+        const hoverStyle = document.getElementById("hover-style");
+        if (hoverStyle) {
+          hoverStyle.remove();
+        }
+      }
+    },
+  },
+};
 </script>
 
 <style>
--- ruoyi-ui/src/layout/components/Sidebar/Logo.vue
+++ ruoyi-ui/src/layout/components/Sidebar/Logo.vue
@@ -1,45 +1,80 @@
 <template>
-  <div class="sidebar-logo-container" :class="{'collapse':collapse}" :style="{ backgroundColor: sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground }">
+  <div
+    class="sidebar-logo-container"
+    :class="{ collapse: collapse }"
+    :style="{
+      backgroundColor:
+        sideTheme === 'theme-dark'
+          ? variables.menuBackground
+          : variables.menuLightBackground,
+    }"
+  >
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+      <router-link
+        v-if="collapse"
+        key="collapse"
+        class="sidebar-logo-link"
+        to="/"
+      >
         <img v-if="logo" :src="logo" class="sidebar-logo" />
-        <h1 v-else class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }} </h1>
+        <h1
+          v-else
+          class="sidebar-title"
+          :style="{
+            color:
+              sideTheme === 'theme-dark'
+                ? variables.logoTitleColor
+                : variables.logoLightTitleColor,
+          }"
+        >
+          {{ title }}
+        </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo" />
-        <h1 class="sidebar-title" :style="{ color: sideTheme === 'theme-dark' ? variables.logoTitleColor : variables.logoLightTitleColor }">{{ title }} </h1>
+        <h1
+          class="sidebar-title"
+          :style="{
+            color:
+              sideTheme === 'theme-dark'
+                ? variables.logoTitleColor
+                : variables.logoLightTitleColor,
+          }"
+        >
+          {{ title }}
+        </h1>
       </router-link>
     </transition>
   </div>
 </template>
 
 <script>
-import logoImg from '@/assets/logo/logo.png'
-import variables from '@/assets/styles/variables.scss'
+import logoImg from "@/assets/logo/logo.png";
+import variables from "@/assets/styles/variables.scss";
 
 export default {
-  name: 'SidebarLogo',
+  name: "SidebarLogo",
   props: {
     collapse: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
     variables() {
       return variables;
     },
     sideTheme() {
-      return this.$store.state.settings.sideTheme
-    }
+      return this.$store.state.settings.sideTheme;
+    },
   },
   data() {
     return {
       title: process.env.VUE_APP_TITLE,
-      logo: logoImg
-    }
-  }
-}
+      logo: logoImg,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -66,8 +101,8 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+      width: 24px;
+      height: 24px;
       vertical-align: middle;
       margin-right: 12px;
     }
