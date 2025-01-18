287ab4b1783b0006e36cef8fc08f0a40612dc488
chip-cmyk <290806494@qq.com>
2025-01-18 19:31:13 +0800

    改文案和首页

--- ruoyi-ui/src/components/Breadcrumb/index.vue
+++ ruoyi-ui/src/components/Breadcrumb/index.vue
@@ -2,7 +2,11 @@
   <el-breadcrumb class="app-breadcrumb" separator="/">
     <transition-group name="breadcrumb">
       <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
-        <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{ item.meta.title }}</span>
+        <span
+          v-if="item.redirect === 'noRedirect' || index == levelList.length - 1"
+          class="no-redirect"
+          >{{ item.meta.title }}</span
+        >
         <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
       </el-breadcrumb-item>
     </transition-group>
@@ -13,80 +17,86 @@
 export default {
   data() {
     return {
-      levelList: null
-    }
+      levelList: null,
+    };
   },
   watch: {
     $route(route) {
       // if you go to the redirect page, do not update the breadcrumbs
-      if (route.path.startsWith('/redirect/')) {
-        return
+      if (route.path.startsWith("/redirect/")) {
+        return;
       }
-      this.getBreadcrumb()
-    }
+      this.getBreadcrumb();
+    },
   },
   created() {
-    this.getBreadcrumb()
+    this.getBreadcrumb();
   },
   methods: {
     getBreadcrumb() {
       // only show routes with meta.title
-      let matched = []
-      const router = this.$route
-      const pathNum = this.findPathNum(router.path)
+      let matched = [];
+      const router = this.$route;
+      const pathNum = this.findPathNum(router.path);
       // multi-level menu
       if (pathNum > 2) {
-        const reg = /\/\w+/gi
+        const reg = /\/\w+/gi;
         const pathList = router.path.match(reg).map((item, index) => {
-          if (index !== 0) item = item.slice(1)
-          return item
-        })
-        this.getMatched(pathList, this.$store.getters.defaultRoutes, matched)
+          if (index !== 0) item = item.slice(1);
+          return item;
+        });
+        this.getMatched(pathList, this.$store.getters.defaultRoutes, matched);
       } else {
-        matched = router.matched.filter(item => item.meta && item.meta.title)
+        matched = router.matched.filter((item) => item.meta && item.meta.title);
       }
       // 判断是否为首页
       if (!this.isDashboard(matched[0])) {
-        matched = [{ path: "/index", meta: { title: "首页" } }].concat(matched)
+        matched = [{ path: "/index", meta: { title: "首页" } }].concat(matched);
       }
-      this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
+      this.levelList = matched.filter(
+        (item) => item.meta && item.meta.title && item.meta.breadcrumb !== false
+      );
     },
     findPathNum(str, char = "/") {
-      let index = str.indexOf(char)
-      let num = 0
+      let index = str.indexOf(char);
+      let num = 0;
       while (index !== -1) {
-        num++
-        index = str.indexOf(char, index + 1)
+        num++;
+        index = str.indexOf(char, index + 1);
       }
-      return num
+      return num;
     },
     getMatched(pathList, routeList, matched) {
-      let data = routeList.find(item => item.path == pathList[0] || (item.name += '').toLowerCase() == pathList[0])
+      let data = routeList.find(
+        (item) =>
+          item.path == pathList[0] ||
+          (item.name += "").toLowerCase() == pathList[0]
+      );
       if (data) {
-        matched.push(data)
+        matched.push(data);
         if (data.children && pathList.length) {
-          pathList.shift()
-          this.getMatched(pathList, data.children, matched)
+          pathList.shift();
+          this.getMatched(pathList, data.children, matched);
         }
       }
     },
     isDashboard(route) {
-      const name = route && route.name
+      const name = route && route.name;
       if (!name) {
-        return false
+        return false;
       }
-      return name.trim() === 'Index'
+      return name.trim() === "Index";
     },
     handleLink(item) {
-      const { redirect, path } = item
+      const { redirect, path } = item;
       if (redirect) {
-        this.$router.push(redirect)
-        return
+        this.$router.push(redirect);
+        return;
       }
-      this.$router.push(path)
-    }
-  }
-}
+      this.$router.push(path);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
